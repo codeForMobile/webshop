4b56d71707557c75d3c13f752b5d4f95ed2586ae
codeForMobile <code.mobile@protonmail.com>
2023-09-24 19:42:32 -0600

    show added items in cart gui

---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,20 @@
+export const addDecimals = (num) => {
+    return (Math.round(num*100)/100).toFixed(2)
+}
+
+export const updateCart = (state) => {
+    //calculate item price
+    state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => 
+    acc + item.price * item.qty, 0))
+    //calculate shipping price (if order > $100 then free else $10)
+    state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0: 10)
+    //calculate tax price
+    state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2))
+    //calculate total price
+    state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+    ).toFixed(2)
+    localStorage.setItem('cart', JSON.stringify(state))
+}
