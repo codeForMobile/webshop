c7e23b4251d9fb741ad36d0021fe59e2091e81f4
codeForMobile <code.mobile@protonmail.com>
2023-09-28 20:38:09 -0600

    add user routes selection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 import { NotFound, errorHandler } from './middleware/errorHandler.js'
 
 const port = process.env.PORT || 6000
@@ -15,7 +17,7 @@ const app = express()
 app.get('/', (req,res) => res.send('api is running...'))
 
 app.use('/api/products', productRoutes)
-
+app.use('/api/users', userRoutes)
 app.use(NotFound)
 app.use(errorHandler)
 
