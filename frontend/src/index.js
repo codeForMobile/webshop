7d2cc0331ffa0194443d5a9651d2a6557e6abac5
codeForMobile <code.mobile@protonmail.com>
2023-09-30 16:35:54 -0600

    add shipping-address screen markup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,6 +23,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import ShippingScreen from './screens/ShippingScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements([
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen/>}/>
       <Route path='/login' element={<LoginScreen/>}/>
       <Route path='/register' element={<RegisterScreen/>}/>
+      <Route path='/shipping' element={<ShippingScreen/>}/>
     </Route>
   ])
 )
---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,80 @@
+import {useState} from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import FormContainer from '../components/FormContainer'
+import {saveShippingAddress} from '../slices/cartSlice'
+
+const ShippingScreen = () => {
+  const cart = useSelector((state) => state.cart)
+  const { ShippingAddress } = cart
+  const [address, SetAddress] = useState(ShippingAddress?.address || '')
+  const [city, SetCity] = useState(ShippingAddress?.city || '')
+  const [postalCode, SetPostalCode] = useState(ShippingAddress?.postalCode || '')
+  const [country, SetCountry] = useState(ShippingAddress?.country || '')
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(saveShippingAddress({address, city, postalCode, country}))
+    navigate('/payment')
+  }
+
+  return (
+    <FormContainer>
+        <h1>Shipping</h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='address' className='my-2'>
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter Address'
+                    value={address}
+                    onChange={(e) => SetAddress(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='city' className='my-2'>
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter city'
+                    value={city}
+                    onChange={(e) => SetCity(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='postalCode' className='my-2'>
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter Postal Code'
+                    value={postalCode}
+                    onChange={(e) => SetPostalCode(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='country' className='my-2'>
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='Enter Country'
+                    value={country}
+                    onChange={(e) => SetCountry(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+
+            <Button 
+                type='submit'
+                variant='primary'
+                className='my-2'>
+                    Continue
+            </Button>
+        </Form>
+    </FormContainer>
+  )
+}
+
+export default ShippingScreen
