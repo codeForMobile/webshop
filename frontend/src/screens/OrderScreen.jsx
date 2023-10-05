133a2205929986a64e77003c14cdc9422899843f
codeForMobile <code.mobile@protonmail.com>
2023-10-05 09:48:19 -0600

    add more stuff to order screen

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,60 @@
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, ListGroup, Image, Form, Button, Cart} from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { useGetOrderDetailsQuery } from '../slices/orderApiSlice'
+
+const OrderScreen = () => {
+    const { id: orderId } = useParams()
+    const { data: order, refetch, isLoading, error } = useGetOrderDetailsQuery(orderId)
+
+    return isLoading ? <Loader/> : error ? <Message variant='danger'/> : (
+        <>
+        <h1>Order {order._id}</h1>
+        <Row>
+            <Col md={8}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>Shipping</h2>
+                        <p>
+                            <strong>Name: </strong> {order.user.name}
+                        </p>
+                        <p>
+                            <strong>Email: </strong> {order.user.email}
+                        </p>
+                        <p>
+                            <strong>Address: </strong>
+                            {order.shippingAddress.address}, {order.shippingAddress.city} {
+                                order.shippingAddress.postalCode}, {
+                                    order.shippingAddress.country
+                            }
+                        </p>
+
+                        {
+                            order.isDelivered ? (
+                                <Message variant='success'>
+                                    Delivered on {order.deliveredAt}
+                                </Message>
+                            ) : (
+                                <Message variant='danger'>Not Delivered</Message>
+                            )
+                        }
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <h2>Payment Method</h2>
+                        <p>
+                            <strong>Method: </strong>
+                            {order.paymentMethod}
+                        </p>
+                        
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={4}>Column</Col>
+        </Row>
+        </>
+    )
+}
+
+export default OrderScreen
