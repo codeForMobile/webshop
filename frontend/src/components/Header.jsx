2f69b3b8400cc26efecf5a11bfddaef262bee50c
codeForMobile <code.mobile@protonmail.com>
2024-07-15 15:35:49 +0530

    add admin route initial stuff -- add orderlist page and other admin routes(only)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from 'react-router-dom'
-import {Navbar, Nav, Container, Badge, NavDropdown} from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux'
 import { useLogoutMutation } from '../slices/usersApiSlice'
-import {logout} from '../slices/authSlice'
+import { logout } from '../slices/authSlice'
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -13,7 +13,7 @@ const Header = () => {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const [ logoutApiCall ] = useLogoutMutation()
+  const [logoutApiCall] = useLogoutMutation()
 
   const logoutHandler = async () => {
     try {
@@ -26,48 +26,67 @@ const Header = () => {
   }
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect> 
-      <Container>
-        <LinkContainer to='/'>
-        <Navbar.Brand>
-        <img src={logo} alt="WebShop" />
-        WebShop
-        </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-        <Navbar.Collapse id='basic-navbar-nav'>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='WebShop' />
+              WebShop
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+          <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-                <LinkContainer to='/cart'>
-                <Nav.Link><FaShoppingCart/>Cart
-                {
-                  cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{
-                      marginLeft: '5px'
-                    }}>
-                      {cartItems.reduce((a,c) => a + c.qty, 0)}
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                  {cartItems.length > 0 && (
+                    <Badge
+                      pill
+                      bg='success'
+                      style={{
+                        marginLeft: '5px',
+                      }}
+                    >
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
                     </Badge>
-                  )
-                }
+                  )}
                 </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link href='/login'>
+                    <FaUser />
+                    Sign In
+                  </Nav.Link>
                 </LinkContainer>
-                { userInfo ? (
-                  <NavDropdown title={userInfo.name} id='username'>
-                    <LinkContainer to='/profile'>
-                      <NavDropdown.Item>
-                        Profile
-                      </NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (<LinkContainer to='/login'>
-                <Nav.Link href='/login'>
-                  <FaUser/>Sign In
-                </Nav.Link>
-                </LinkContainer>)}
+              )}
+
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
-        </Navbar.Collapse>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
+  RouterProvider,
 } from 'react-router-dom'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { Provider } from 'react-redux'
@@ -17,50 +17,54 @@ import store from './store.js'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PrivateRoute from './components/PrivateRoute.jsx';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen.jsx';
-import ShippingScreen from './screens/ShippingScreen.jsx';
-import PaymentScreen from './screens/PaymentScreen.jsx';
-import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
-import OrderScreen from './screens/OrderScreen.jsx';
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import AdminRoute from './components/AdminRoute.jsx'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen.jsx'
+import ShippingScreen from './screens/ShippingScreen.jsx'
+import PaymentScreen from './screens/PaymentScreen.jsx'
+import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx'
+import OrderScreen from './screens/OrderScreen.jsx'
 import ProfileScreen from './screens/ProfileScreen.jsx'
+import OrderListScreen from './screens/admin/OrderListScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
-    <Route path='/' element={<App/>}>
-      <Route index={true} path='/' element={<HomeScreen/>}/>
-      <Route path='/product/:id' element={<ProductScreen/>}/>
-      <Route path='/cart' element={<CartScreen/>}/>
-      <Route path='/login' element={<LoginScreen/>}/>
-      <Route path='/register' element={<RegisterScreen/>}/>
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
 
-      <Route path='' element={<PrivateRoute/>}>
-        <Route path='/shipping' element={<ShippingScreen/>}/>
-        <Route path='/payment' element={<PaymentScreen/>}/>
-        <Route path='/placeorder' element={<PlaceOrderScreen/>}/>
-        <Route path='/order/:id' element={<OrderScreen/>}/>
-        <Route path='/profile' element={<ProfileScreen/>}/>
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+        <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
+        <Route path='/profile' element={<ProfileScreen />} />
       </Route>
 
-    </Route>
-  ])
+      <Route path='' element={<AdminRoute />}>
+        <Route path='admin/orderlist' element={<OrderListScreen />} />
+      </Route>
+    </Route>,
+  ]),
 )
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PayPalScriptProvider deferLoading={true}>
-            <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </PayPalScriptProvider>
     </Provider>
-  </React.StrictMode>
-);
-reportWebVitals();
-
+  </React.StrictMode>,
+)
+reportWebVitals()
