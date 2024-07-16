fd67e2986f16c20f0fb4e1ce524a70ef131f174d
codeForMobile <code.mobile@protonmail.com>
2024-07-16 13:13:14 +0530

    create product works in admin screen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,23 @@ const getProductById = asyncHandler(async (req,res) =>{
     }
 })
 
-export { getProductById, getProducts }
+//  @desc    Create new product (dummy)
+//  @route   Post api/products
+//  @access  Private/Admin
+const createProduct = asyncHandler(async (req,res) =>{
+    const product = new Product({
+        name: 'sample product',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description'
+    })
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+export { getProductById, getProducts, createProduct }
